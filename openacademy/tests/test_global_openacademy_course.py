c7ea281a3467a19d3819b28db315cf89961f2571
Micaias <micaias.hinojosa@grupoabsa.com>
2018-11-20 19:36:49 +0000

    [IMP] openacademy: Adding basic unittest

--- openacademy/tests/test_global_openacademy_course.py
+++ openacademy/tests/test_global_openacademy_course.py
@@ -4,6 +4,8 @@ from psycopg2 import IntegrityError
 
 from odoo.tests.common import TransactionCase
 
+from odoo.tools import mute_logger
+
 class GlobalTestOpenAcademyCourse(TransactionCase):
     '''
     Global test to openacademy course model.
@@ -18,6 +20,7 @@ class GlobalTestOpenAcademyCourse(TransactionCase):
 
     # Method of class that don't is test
     def create_course(self, course_name, course_description, course_responsible_id):
+        #create a course with parameters received
         course_id = self.course.create({
             'name' : course_name,
             'description' : course_description,
@@ -26,15 +29,21 @@ class GlobalTestOpenAcademyCourse(TransactionCase):
         return course_id
 
     # Method of test starts with 'def test_*(self):'
+
+    # Mute the error odoo.sql_db to avoid it in log
+    @mute_logger('odoo.sql_db')
     def test_01_same_name_description(self):
         '''
         Test create a course with same name and description.
         To test constraint of name different to description.
         '''
+        #Error raised expected with message expected
         with self.assertRaisesRegexp(
                 IntegrityError,
-                'msg de error'
+                'new row for relation "openacademy_course" violates'
+                ' check constraint "openacademy_course_name_description_check"'
                 ):
+            # Create a course with same and description to raise error.
             self.create_course('test_name','test_description',None)
 
 
