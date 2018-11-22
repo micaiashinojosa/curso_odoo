3b1e15b42a64df14363d2d000e13d50a87e8e8aa
Micaias <micaias.hinojosa@grupoabsa.com>
2018-11-22 16:46:49 +0000

    [REF] openacademy: Add duplicate test to openacademy course

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -30,6 +30,7 @@ class Course(models.Model):
     ]
 
     def copy(self, default=None):
+        print("estoy pasandi por la funcion heredada de copy en cursos")
         if default is None:
             default = {}
         copied_count = self.search_count([
